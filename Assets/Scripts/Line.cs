8307e6e0e9ec303e6e9e0783949bb77c5243aa0d
Thieu Mao <thieumao@gmail.com>
2020-10-18 13:58:49 +0700

    Moving

--- Assets/Scripts/Line.cs
+++ Assets/Scripts/Line.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Line : MonoBehaviour
 {
+    public float moveSpeed;
+    float xDirection;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,8 @@ public class Line : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        xDirection = Input.GetAxisRaw("Horizontal");
+        float moveStep = moveSpeed * xDirection * Time.deltaTime;
+        transform.position = transform.position + new Vector3(moveSpeed, 0, 0);
     }
 }
